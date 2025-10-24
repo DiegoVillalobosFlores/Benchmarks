1c3e7c23ad39682e1c33057eaf59d89782e883f9
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-24 11:41:05 +0200

    Add support for woff2 files and add gzip encoding support

--- src/entrypoints/index.ts
+++ src/entrypoints/index.ts
@@ -8,7 +8,7 @@ const entrypoints = readdirSync("./src/entrypoints", { withFileTypes: true })
       !entry.isDirectory() &&
       entry.name !== "index.ts" &&
       entry.name !== "favicon.svg" &&
-      entry.name !== "font.ttf",
+      entry.name !== "font.woff2",
   )
   .reverse()
   .map((entry) => {
@@ -18,7 +18,7 @@ const entrypoints = readdirSync("./src/entrypoints", { withFileTypes: true })
 export const assetMap: AppAssetMap = {
   globalStyles: "globalStyles.css",
   favicon: "favicon.svg",
-  font: "font.ttf",
+  font: "font.woff2",
   ...Object.entries(pagesRouter).reduce(
     (acc, [key, value]) => {
       acc[key] = value.hydrationScript;
@@ -33,33 +33,39 @@ const contentTypeMap = {
   js: "application/javascript",
   svg: "image/svg+xml",
   ttf: "font/ttf",
+  woff2: "font/woff2",
 };
 
-export const assetMapStaticFiles = Object.values<string>(assetMap).reduce(
-  (acc, assetName) => {
-    const assetExtension = assetName.split(".").pop() as
-      | keyof typeof contentTypeMap
-      | undefined;
+export const assetMapStaticFiles = async () => {
+  const staticFiles = await Promise.all(
+    Object.values<string>(assetMap).map(async (assetName) => {
+      const assetExtension = assetName.split(".").pop() as
+        | keyof typeof contentTypeMap
+        | undefined;
 
-    if (!assetExtension) {
-      throw new Error(`Invalid asset name: ${assetName}`);
-    }
+      if (!assetExtension) {
+        throw new Error(`Invalid asset name: ${assetName}`);
+      }
 
-    if (!contentTypeMap[assetExtension]) {
-      throw new Error(`Unsupported asset type: ${assetExtension}`);
-    }
+      if (!contentTypeMap[assetExtension]) {
+        throw new Error(`Unsupported asset type: ${assetExtension}`);
+      }
 
-    return {
-      ...acc,
-      [`/${assetName}`]: async () =>
-        new Response(await Bun.file(`dist/${assetName}`).bytes(), {
-          headers: {
-            "Content-Type": contentTypeMap[assetExtension],
+      return {
+        [`/${assetName}`]: new Response(
+          await Bun.file(`dist/${assetName}`).bytes(),
+          {
+            headers: {
+              "Content-Type": contentTypeMap[assetExtension],
+              "Content-Encoding": "gzip",
+            },
           },
-        }),
-    };
-  },
-  {},
-);
+        ),
+      };
+    }),
+  );
+
+  return Object.assign({}, ...staticFiles);
+};
 
 export default entrypoints;
