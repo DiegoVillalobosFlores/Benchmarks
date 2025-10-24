16870246f2477975965afd05ad038c8efb3f1d0a
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-24 11:46:58 +0200

    Add inline styles to PageBody

--- src/components/Page/PageBody.tsx
+++ src/components/Page/PageBody.tsx
@@ -3,5 +3,19 @@ type Props = {
 };
 
 export default function PageBody({ children }: Props) {
-  return <body>{children}</body>;
+  return (
+    <body
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100dvh",
+        backgroundColor: "black",
+        color: "white",
+        padding: "32px",
+        gap: "16px",
+      }}
+    >
+      {children}
+    </body>
+  );
 }
