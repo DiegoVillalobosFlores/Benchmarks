d6bf4c5dd70fd0613d87497b67b69cb30d1a81a5
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-25 21:46:14 +0200

    Move entrypoints generation to buildClientBundle

--- src/utils/buildClientBundle.ts
+++ src/utils/buildClientBundle.ts
@@ -1,11 +1,38 @@
-import entrypoints from "@/entrypoints";
 import AppAssetMap from "@/types/AppAssetMap";
 import AppRouteScripts from "@/types/AppRouteScripts";
+import { readdirSync } from "fs";
+import contentTypeMap from "./contentTypeMap";
 
 export default async function buildClientBundle(): Promise<{
   scripts: AppRouteScripts;
   assets: AppAssetMap;
 }> {
+  await Bun.write("./dist/_init", "");
+  await Bun.write("./cache/_init", "");
+  await Bun.write("./build/_init", "");
+
+  const entrypoints = readdirSync("./src/entrypoints", { withFileTypes: true })
+    .filter((entry) => !entry.isDirectory())
+    .reduce(
+      (acc, entry) => {
+        const extension = entry.name.split(".").pop() as
+          | keyof typeof contentTypeMap
+          | undefined;
+        if (!extension) {
+          throw new Error(`Invalid asset name: ${entry.name}`);
+        }
+
+        return {
+          ...acc,
+          [extension]: [
+            ...(acc[extension] || []),
+            `${entry.parentPath}/${entry.name}`,
+          ],
+        };
+      },
+      {} as Record<string, string[]>,
+    );
+
   const bundledEntrypoints = Object.values<string[]>(entrypoints);
 
   const scripts: AppRouteScripts = {
@@ -28,6 +55,10 @@ export default async function buildClientBundle(): Promise<{
       splitting: true,
       packages: "bundle",
       target: "browser",
+      env: "inline",
+      define: {
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      },
     });
 
     const chunks = result.outputs.filter((output) => output.kind === "chunk");
@@ -66,6 +97,10 @@ export default async function buildClientBundle(): Promise<{
         buildAssets[0]?.path.split("/").pop();
     }
   }
+
+  const clientBundleManifest = Bun.file("./build/manifest.json");
+
+  clientBundleManifest.write(JSON.stringify({ scripts, assets }, null, 2));
 
   return {
     scripts,
