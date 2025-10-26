22ab22c56d1ec68f3f9e32e2d950beba39fbfd37
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-26 14:56:45 +0100

    Refactor services index to require a sqlClient

--- src/core/services/benchmarks/index.ts
+++ src/core/services/benchmarks/index.ts
@@ -1,13 +1,10 @@
 import { SQL } from "bun";
 import BenchmarksService from "./benchmarks";
-import SQLiteClient from "@/core/clients/sql/sqlite";
 
 export default async function BenchmarksServiceInstance({
   sqlClient,
 }: {
-  sqlClient?: SQL;
+  sqlClient: SQL;
 }) {
-  const client = sqlClient || (await SQLiteClient());
-
-  return BenchmarksService({ sqlClient: client });
+  return BenchmarksService({ sqlClient });
 }
