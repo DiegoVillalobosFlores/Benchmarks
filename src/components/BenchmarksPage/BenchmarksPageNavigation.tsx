5046869562674e8ca63fdf66a13276bf132b5c58
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-26 18:03:38 +0100

    Refactor BenchmarksPage to use new Page composition

--- src/components/BenchmarksPage/BenchmarksPageNavigation.tsx
+++ src/components/BenchmarksPage/BenchmarksPageNavigation.tsx
@@ -1,13 +1,13 @@
 import {
+  ChevronDown,
+  ChevronUp,
   CornerDownRight,
-  MoveDown,
   MoveLeft,
-  MoveRight,
-  MoveUp,
 } from "lucide-react";
 import PageNavigation from "../Page/PageNavigation";
 import PageNavigationList from "../Page/PageNavigationList";
 import PageNavigationControl from "../Page/PageNavigationControl";
+import { ReactNode } from "react";
 
 export type Control =
   | "categories"
@@ -25,6 +25,53 @@ type Props = {
   onClick: (control: Control) => void;
 };
 
+type NavigationControl = {
+  id: Control;
+  label: string;
+  icon: ReactNode;
+};
+
+const navigationControls: NavigationControl[] = [
+  {
+    id: "categories",
+    label: "Categories",
+    icon: <>1</>,
+  },
+  {
+    id: "benchmarks",
+    label: "Benchmarks",
+    icon: <>2</>,
+  },
+  {
+    id: "upload",
+    label: "Upload",
+    icon: <>3</>,
+  },
+];
+
+const selectionControls: NavigationControl[] = [
+  {
+    id: "moveDown",
+    label: "Next",
+    icon: <ChevronDown />,
+  },
+  {
+    id: "moveUp",
+    label: "Previous",
+    icon: <ChevronUp />,
+  },
+  {
+    id: "moveBack",
+    label: "Go Back",
+    icon: <MoveLeft />,
+  },
+  {
+    id: "go",
+    label: "Select",
+    icon: <CornerDownRight />,
+  },
+];
+
 export default function BenchmarksPageNavigation({
   activeControl,
   focusedControl,
@@ -34,70 +81,32 @@ export default function BenchmarksPageNavigation({
   return (
     <PageNavigation>
       <PageNavigationList title="Navigation:">
-        <PageNavigationControl
-          onClick={() => onClick("categories")}
-          isActive={activeControl === "categories"}
-          isFocused={focusedControl === "categories"}
-          onFocus={() => onFocus("categories")}
-        >
-          <h3>1. Categories</h3>
-        </PageNavigationControl>
-        <PageNavigationControl
-          onClick={() => onClick("benchmarks")}
-          isActive={activeControl === "benchmarks"}
-          isFocused={focusedControl === "benchmarks"}
-          onFocus={() => onFocus("benchmarks")}
-        >
-          <h3>2. Benchmarks</h3>
-        </PageNavigationControl>
-        <PageNavigationControl
-          onClick={() => onClick("upload")}
-          isActive={activeControl === "upload"}
-          isFocused={focusedControl === "upload"}
-          onFocus={() => onFocus("upload")}
-        >
-          <h3>3. Upload</h3>
-        </PageNavigationControl>
+        {navigationControls.map((control) => (
+          <PageNavigationControl
+            key={control.id}
+            onClick={() => onClick(control.id)}
+            isActive={activeControl === control.id}
+            isFocused={focusedControl === control.id}
+            onFocus={() => onFocus(control.id)}
+            icon={control.icon}
+          >
+            {control.label}
+          </PageNavigationControl>
+        ))}
       </PageNavigationList>
-      <PageNavigationList title="Controls:">
-        <PageNavigationControl
-          onClick={() => onClick("moveBack")}
-          isActive={activeControl === "moveBack"}
-          isFocused={focusedControl === "moveBack"}
-          onFocus={() => onFocus("moveBack")}
-        >
-          <MoveLeft size={16} />
-          <h3>Move Back</h3>
-        </PageNavigationControl>
-        <PageNavigationControl
-          onClick={() => onClick("go")}
-          isActive={activeControl === "go"}
-          isFocused={focusedControl === "go"}
-          onFocus={() => onFocus("go")}
-        >
-          <MoveRight size={16} />
-          <h3>|</h3>
-          <CornerDownRight size={16} />
-          <h3>Go</h3>
-        </PageNavigationControl>
-        <PageNavigationControl
-          onClick={() => onClick("moveUp")}
-          isActive={activeControl === "moveUp"}
-          isFocused={focusedControl === "moveUp"}
-          onFocus={() => onFocus("moveUp")}
-        >
-          <MoveUp size={16} />
-          <h3>Move Up</h3>
-        </PageNavigationControl>
-        <PageNavigationControl
-          onClick={() => onClick("moveDown")}
-          isActive={activeControl === "moveDown"}
-          isFocused={focusedControl === "moveDown"}
-          onFocus={() => onFocus("moveDown")}
-        >
-          <MoveDown size={16} />
-          <h3>Move Down</h3>
-        </PageNavigationControl>
+      <PageNavigationList title="Selection:">
+        {selectionControls.map((control) => (
+          <PageNavigationControl
+            key={control.id}
+            onClick={() => onClick(control.id)}
+            isActive={activeControl === control.id}
+            isFocused={focusedControl === control.id}
+            onFocus={() => onFocus(control.id)}
+            icon={control.icon}
+          >
+            {control.label}
+          </PageNavigationControl>
+        ))}
       </PageNavigationList>
     </PageNavigation>
   );
--- src/components/Page/PageHeader.tsx
+++ src/components/Page/PageHeader.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 type Props = {
   title: string;
-  children: ReactNode;
+  children?: ReactNode;
 };
 
 export default function PageHeader({ title, children }: Props) {
