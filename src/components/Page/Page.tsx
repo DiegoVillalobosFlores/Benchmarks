c5f0e38252df96058512bb08ee7d4d2fe57dc191
Diego Villalobos <diego.villalobos@service-hero.com>
2025-11-01 00:11:13 +0100

    Refactor Page component to now use an array of assets and to minify the critical css

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,15 +1,17 @@
-import AppAssetMap from "@/types/AppAssetMap";
 import { ReactNode } from "react";
 import PageBody from "./PageBody";
 import PageHRM from "./PageHRM";
 
 type Props = {
-  assetMap: AppAssetMap;
   title: string;
   children: ReactNode;
+  assetLinks: string[];
 };
 
-export default function Page({ assetMap, children, title }: Props) {
+const styles =
+  "*{box-sizing:border-box;font-optical-sizing:auto;margin:0;padding:0;font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Noto Sans,Ubuntu,Cantarell,Helvetica Neue;font-style:normal;font-weight:200}body{display:flex;color:#fff;background-color:#000;flex-direction:column;gap:16px;height:100dvh;padding:32px}main{display:contents;height:100%}section{display:contents}ul{list-style-type:none}";
+
+export default function Page({ children, title, assetLinks }: Props) {
   return (
     <html lang="en">
       <head>
@@ -19,55 +21,15 @@ export default function Page({ assetMap, children, title }: Props) {
           name="description"
           content="Simple site to view, store and share all the benchmarks from different sources"
         />
-        <link rel="preconnect" href={assetMap.font} as="font" />
-        <link rel="icon" type="image/svg+xml" href={assetMap.favicon} />
-        <link rel="preconnect" href={assetMap.globalStyles} as="style" />
-        <style>{`
-          @font-face {
-              font-family: "Quicksand";
-              font-optical-sizing: auto;
-              font-weight: 200;
-              font-style: normal;
-              font-display: swap;
-              src: url(${assetMap.font}) format(woff2);
-          }
-
-          * {
-              margin: 0;
-              padding: 0;
-              box-sizing: border-box;
-              font-family: system-ui;
-              font-optical-sizing: auto;
-              font-style: normal;
-              font-weight: 200;
-          }
-
-          body {
-              display: flex;
-              flex-direction: column;
-              height: 100dvh;
-              background-color: black;
-              color: white;
-              padding: 32px;
-              gap: 16px;
-          }
-
-          main {
-              height: 100%
-          }
-
-          main {
-              display: contents;
-          }
-
-          section {
-              display: contents;
-          }
-
-          ul {
-              list-style-type: none;
-          }
-          `}</style>
+        <link rel="icon" type="image/svg+xml" href={"favicon.svg"} />
+        {assetLinks.map((link) => (
+          <link
+            key={link}
+            rel={link.endsWith(".css") ? "stylesheet" : "preload"}
+            href={link}
+          />
+        ))}
+        <style>{styles}</style>
         <title>{title}</title>
       </head>
       <PageBody>{children}</PageBody>
