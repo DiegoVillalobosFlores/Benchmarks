233c830fc5c5bfdf5be38b69e984c8b1e915d90a
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-31 23:47:08 +0100

    Refactor static files routes to serve all files in dist

--- src/routes/static/files.ts
+++ src/routes/static/files.ts
@@ -2,40 +2,60 @@ import contentTypeMap from "@/utils/contentTypeMap";
 import { readdirSync } from "fs";
 
 export const staticFilesRoutes = async () => {
-  const staticFiles = readdirSync("./dist", { withFileTypes: true }).filter(
-    (entry) => entry.isFile(),
+  console.time("reading dir");
+  const staticFiles = readdirSync("./dist", {
+    withFileTypes: true,
+    recursive: true,
+  })
+    .filter((entry) => entry.isFile())
+    .map((entry) => `${entry.parentPath}/${entry.name}`);
+  console.timeEnd("reading dir");
+
+  const enableCompression = process.env.ENABLE_ASSET_COMPRESSION
+    ? process.env.ENABLE_ASSET_COMPRESSION === "true"
+    : true;
+
+  const promises = staticFiles.map(
+    (staticFile) =>
+      new Promise<{ url: string; response: Response }>(async (resolve) => {
+        console.time(`reading file ${staticFile}`);
+        const readFile = await Bun.file(staticFile).bytes();
+        console.timeEnd(`reading file ${staticFile}`);
+
+        const response = enableCompression
+          ? Bun.zstdCompressSync(readFile, {
+              level: 3,
+            })
+          : readFile;
+
+        const headers: HeadersInit = {
+          "Content-Type":
+            contentTypeMap[
+              staticFile.split(".").pop() as keyof typeof contentTypeMap
+            ],
+        };
+
+        if (enableCompression) {
+          headers["Content-Encoding"] = "zstd";
+        }
+
+        resolve({
+          url: `/${staticFile}`,
+          response: new Response(response, { headers }),
+        });
+      }),
   );
 
-  const router: Record<string, Response> = {};
+  console.time("compressing files");
+  const results = await Promise.all(promises);
 
-  const enableCompression = true;
+  console.timeEnd("compressing files");
 
-  for (const entry of staticFiles) {
-    const readFile = await Bun.file(`dist/${entry.name}`).bytes();
-
-    const response = enableCompression
-      ? Bun.gzipSync(readFile, {
-          level: 9,
-          memLevel: 9,
-          windowBits: 31,
-        })
-      : readFile;
-
-    const headers: HeadersInit = {
-      "Content-Type":
-        contentTypeMap[
-          entry.name.split(".").pop() as keyof typeof contentTypeMap
-        ],
-    };
-
-    if (enableCompression) {
-      headers["Content-Encoding"] = "gzip";
-    }
-
-    router[`/${entry.name}`] = new Response(response, {
-      headers,
-    });
-  }
-
-  return router;
+  return results.reduce(
+    (acc, result) => {
+      acc[result.url] = result.response;
+      return acc;
+    },
+    {} as Record<string, Response>,
+  );
 };
