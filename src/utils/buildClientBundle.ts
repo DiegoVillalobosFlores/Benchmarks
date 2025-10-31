73ffcfa5f3c0d3111a15259a4a4caf5a92ae25d8
Diego Villalobos <diego.villalobos@service-hero.com>
2025-10-31 23:49:34 +0100

    Refactor buildClientBundle to read from the routes/app folder and create the build output from the .page.tsx files

--- src/utils/buildClientBundle.ts
+++ src/utils/buildClientBundle.ts
@@ -1,109 +1,74 @@
-import AppAssetMap from "@/types/AppAssetMap";
-import AppRouteScripts from "@/types/AppRouteScripts";
 import { readdirSync } from "fs";
-import contentTypeMap from "./contentTypeMap";
+import getHydrationScript from "./getHydrationScript";
+import BuildManifest from "@/types/BundleManifest";
 
-export default async function buildClientBundle(): Promise<{
-  scripts: AppRouteScripts;
-  assets: AppAssetMap;
-}> {
+export default async function buildClientBundle(): Promise<BuildManifest> {
   await Bun.write("./dist/_init", "");
   await Bun.write("./cache/_init", "");
   await Bun.write("./build/_init", "");
 
-  const entrypoints = readdirSync("./src/entrypoints", { withFileTypes: true })
-    .filter((entry) => !entry.isDirectory())
-    .reduce(
-      (acc, entry) => {
-        const extension = entry.name.split(".").pop() as
-          | keyof typeof contentTypeMap
-          | undefined;
-        if (!extension) {
-          throw new Error(`Invalid asset name: ${entry.name}`);
-        }
+  const appPages = readdirSync("./src/routes/app", {
+    withFileTypes: true,
+    recursive: true,
+  })
+    .filter((entry) => !entry.isDirectory() && entry.name.includes(".page.tsx"))
+    .map((entry) => `${entry.parentPath}/${entry.name}`);
 
-        return {
-          ...acc,
-          [extension]: [
-            ...(acc[extension] || []),
-            `${entry.parentPath}/${entry.name}`,
-          ],
-        };
-      },
-      {} as Record<string, string[]>,
-    );
+  const appEntryppoints: string[] = [];
+  const appPagePaths: string[] = [];
 
-  const bundledEntrypoints = Object.values<string[]>(entrypoints);
+  for (const page of appPages) {
+    const hydrationScript = getHydrationScript(page);
+    await Bun.write(`./build/${page}`, hydrationScript);
 
-  const scripts: AppRouteScripts = {
-    root: [],
-    upload: [],
-  };
-
-  const assets: AppAssetMap = {
-    favicon: "",
-    globalStyles: "",
-    font: "",
-  } as const;
-
-  for (const extensionBundle of bundledEntrypoints) {
-    const result = await Bun.build({
-      entrypoints: extensionBundle,
-      minify: true,
-      outdir: "dist",
-      sourcemap: "linked",
-      splitting: true,
-      packages: "bundle",
-      target: "browser",
-      env: "inline",
-      define: {
-        "process.env.NODE_ENV": JSON.stringify("production"),
-      },
-    });
-
-    const chunks = result.outputs.filter((output) => output.kind === "chunk");
-
-    for (const scriptExtension of extensionBundle) {
-      if (!scriptExtension.endsWith(".tsx")) continue;
-      const scriptName = scriptExtension.split("/").pop() as string;
-      const scriptNameWithoutExtension = scriptName.split(".")[0];
-
-      if (!scriptNameWithoutExtension) continue;
-      if (
-        !scriptNameWithoutExtension.startsWith("root") &&
-        !scriptNameWithoutExtension.startsWith("upload")
-      )
-        continue;
-
-      scripts[scriptNameWithoutExtension as keyof typeof scripts] = [
-        `${scriptNameWithoutExtension}.js`,
-        ...chunks.map((chunk) => chunk.path.split("/").pop() as string),
-      ];
-    }
-
-    for (const assetExtensions of extensionBundle) {
-      if (assetExtensions.endsWith(".tsx")) continue;
-      const assetName = assetExtensions.split("/").pop() as string;
-      const assetNameWithoutExtension = assetName.split(".")[0];
-
-      const buildAssets = result.outputs.filter(
-        (output) => output.kind === "asset",
-      );
-
-      if (!assetNameWithoutExtension) continue;
-      if (!Object.keys(assets).includes(assetNameWithoutExtension)) continue;
-
-      assets[assetNameWithoutExtension as keyof typeof assets] =
-        buildAssets[0]?.path.split("/").pop();
-    }
+    appEntryppoints.push(`./build/${page}`);
+    appPagePaths.push(page.split("app/").pop()?.split(".")[0]);
   }
 
-  const clientBundleManifest = Bun.file("./build/manifest.json");
+  const result = await Bun.build({
+    entrypoints: appEntryppoints,
+    minify: true,
+    outdir: "dist",
+    sourcemap: "linked",
+    splitting: true,
+    packages: "bundle",
+    target: "browser",
+    env: "inline",
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
+  });
 
-  clientBundleManifest.write(JSON.stringify({ scripts, assets }, null, 2));
+  // We konw that these chunks are used for all the pages
+  const chunks = result.outputs
+    .filter((output) => output.kind === "chunk")
+    .map((chunk) => chunk.path);
 
-  return {
-    scripts,
-    assets,
-  };
+  // CSS, etc...
+  const buildAssets = result.outputs
+    .filter((output) => output.kind === "asset")
+    .map((asset) => asset.path);
+
+  const bundleManifest = result.outputs
+    .filter((output) => output.kind === "entry-point")
+    .reduce((acc, output) => {
+      const outputKey = output.path.split(".")[0] as string;
+      const appPageKey = output.path.split("dist/").pop().split(".")[0];
+
+      // log(distPath);
+      acc[appPageKey] = {
+        scripts: [
+          ...chunks.map((chunk) => `/dist/${chunk.split("dist/").pop()}`),
+          `/dist/${output.path.split("dist/").pop()}`,
+        ],
+        assets: buildAssets
+          .filter((asset) => asset.startsWith(outputKey))
+          .map((asset) => `/dist/${asset.split("dist/").pop()}`),
+      };
+      return acc;
+    }, {} as BuildManifest);
+
+  Bun.write("./build/manifest.json", JSON.stringify(bundleManifest, null, 2));
+
+  return bundleManifest;
 }
